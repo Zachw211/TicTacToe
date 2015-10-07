7c934acdccb4461dfaf4cbad192f67b2000945fc
Zach Weinstein <zweinstein@gmail.com>
2015-10-06 18:04:00 -0600

    Added Player Class

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,30 +1,47 @@
 class TicTacToe
-	
+
+	attr_accessor :p1_name, :p2_name
+
 	def initialize
 		@board = []
-		9.times {@board << "-"}
-		@turn = 0
+		9.times {@board << "-"}  #create 9 empty board space
+		@turn = 0				#variable to track the turn number
+		@p1 = Player.new("X")
+		@p2 = Player.new("O")
 
 	end
 
-	def display
-		puts
-		puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-		puts "___________"
-		puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-		puts "___________"
-		puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
-		puts
-		
+
+		def display
+			puts
+			puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+			puts "___________"
+			puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+			puts "___________"
+			puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
+			puts
+			
+		end
+
+		def move
+			puts "Where would you like to move? (spaces 1-9)"
+			move = gets.chomp
+
+			@board[move.to_i-1] = @p2.symbol
+
+			display
+		end
 	end
 
-	def move
-		puts "Where would you like to move? (spaces 1-9)"
-		move = gets.chomp
+	class Player
 
-		@board[move.to_i-1] = "x"
+		attr_accessor :name, :symbol
 
-		display
+		def initialize(symbol)
+			@name = nil
+			@symbol = symbol
+		end
+
 	end
 end
 
