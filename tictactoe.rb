2d04ce924bdb812e7fe814d5abfa5c9577b4171f
Zach Weinstein <zweinstein@gmail.com>
2015-10-08 16:21:43 -0600

    Added turns and started play method

--- tictactoe.rb
+++ tictactoe.rb
@@ -8,29 +8,46 @@ class TicTacToe
 		@turn = 0				#variable to track the turn number
 		@p1 = Player.new("X")
 		@p2 = Player.new("O")
+		@playing = true
 
 	end
 
 
-		def display
-			puts
-			puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-			puts "___________"
-			puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-			puts "___________"
-			puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
-			puts
+	def display
+		puts
+		puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+		puts "___________"
+		puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+		puts "___________"
+		puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
+		puts
 			
-		end
+	end
 
-		def move
-			puts "Where would you like to move? (spaces 1-9)"
-			move = gets.chomp
+	def turn
+		@turn % 2 == 0 ? "X" : "O"
+	end
 
-			@board[move.to_i-1] = @p2.symbol
+	def move
+		puts "Where would you like to move? (spaces 1-9)"
+		move = gets.chomp.to_i
+		whose = turn
+		@board[move-1] = turn
+		@turn +=1
 
-			display
-		end
+		display
+	end
+
+
+	def play
+		puts "Welcome to command line Tic Tac Toe."
+		puts "Please enter Player One's name:"
+		@p1.name = gets.chomp
+		puts "Thank you #{@p1.name}. You are X."
+		puts "Please enter Player Two's name:"
+		@p2.name = gets.chomp
+		puts "Thank you #{@p2.name}. You are O."
+	
 	end
 
 	class Player
@@ -47,6 +64,6 @@ end
 
 test = TicTacToe.new
 
-
-
+test.play
+test.move
 test.move
